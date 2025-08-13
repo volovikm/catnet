381fcd8051ae304667eafce9d6e48b6541b4901f
Michael <148370088+volovikm@users.noreply.github.com>
2025-08-13 18:03:27 +0300

    modified moving system

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
     $rows=10;
     $columns=10;
+
+    $commands=[
+        'food'=>['description'=>'Еда'],
+        'drink'=>['description'=>'Пить'],
+        'tastes'=>['description'=>'Вкусняшки'],
+    ];
 ?>
 <html>
     <head>
@@ -9,33 +15,49 @@
         <link rel="stylesheet" href="/web/css/index.css">
     </head>
     <body>
-
-        <div class="container">
-            <table class="field">
-                <?php for($i=10;$i>=1;$i--) :?>
-                    <tr class="row">
-                        <?php for($j=1;$j<=$columns;$j++) :?>
-                            <td class="column" i="<?=$i?>" j="<?=$j?>">
-                                <div class="align-center">
-                                    <?=$i.' - '.$j?>
-                                </div>
-                            </td>
-                        <?php endfor; ?>
-                    </tr>
-                <?php endfor; ?>
-            </table>
+        <div class="container nowrap">
+            <div class="d-inline-block">
+                <table id="field" class="field">
+                    <?php for($i=$rows;$i>=1;$i--) :?>
+                        <tr class="row">
+                            <?php for($j=1;$j<=$columns;$j++) :?>
+                                <td class="column" i="<?=$i?>" j="<?=$j?>">
+                                    <div object class="align-center">
+                                        <?=$i.' - '.$j?>
+                                    </div>
+                                </td>
+                            <?php endfor; ?>
+                        </tr>
+                    <?php endfor; ?>
+                </table>
+            </div>
+            <div class="d-inline-block w-25">
+                <?php foreach($commands as $id=>$command) :?>
+                    <div class="mt-15">
+                        <button command-button command-id="<?= $id ?>" class="button-primary w-100" type="button"><?= $command['description'] ?></button>
+                    </div>
+                <?php endforeach; ?>
+                <div class="mt-15">
+                    <form id="form" class="block p-10">
+                        <div>
+                            <select id="object-input" class="input-base w-100">
+                                <?php foreach($commands as $id=>$command) :?>
+                                    <option value="<?= $id ?>"><?= $command['description'] ?></option>
+                                <?php endforeach; ?>
+                            </select>
+                        </div>
+                        <div class="mt-5">
+                            <button id="change-position-button" class="button-primary w-100" type="button">Изменить положение</button>
+                        </div>
+                        <div class="mt-5">
+                            <button id="find-way-button" class="button-primary w-100" type="button">Найти путь</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
-
-        <div class="align-center w-50 mt-15">
-            <input placeholder="Команда Лютику"></input>
-            <button id="apply-button" class="button-primary w-50" type="button">Применить</button>
-        </div>
-        
     </body>
 
     <script src="web/js/index.js"></script>
-    <script src="web/js/training-data.js"></script>
-    <script src="web/js/train.js"></script>
-    <script src="web/js/net.js"></script>
-    
+
 </html>
--- web/js/index.js
+++ web/js/index.js
@@ -1,3 +1,11 @@
+function randomInteger(min, max) {
+  let rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+}
+
+
+
+//Установка объектов
 function setCatToCoordinates(i,j)
 {
     $('[i="'+i+'"][j="'+j+'"]').addClass('cat');
@@ -13,5 +21,129 @@ function setCatEatingToCoordinates(i,j)
     $('[i="'+i+'"][j="'+j+'"]').addClass('cat-eating');
 }
 
-setCatToCoordinates(1,1);
-setFoodToCoordinates(6,7);
+
+
+function getMaxIndex()
+{
+    max_i=1;
+    $('#field').find('td').each(function(){
+        i=Number.parseInt($(this).attr('i'));
+        if(i>max_i)
+        {
+            max_i=i;
+        }
+    });
+
+    max_j=1;
+    $('#field').find('td').each(function(){
+        j=Number.parseInt($(this).attr('j'));
+        if(j>max_j)
+        {
+            max_j=j;
+        }
+    });
+
+    max_index=[];
+    max_index['i']=max_i;
+    max_index['j']=max_j;
+
+    return max_index;
+}
+
+//Изменение положения
+$('#change-position-button').on('click',function(){
+    object=$('#object-input').val();
+
+    $('td[i][j]').removeClass(object);
+
+    min_i=1;
+    min_j=1;
+    
+    max_index=getMaxIndex();
+
+    object_i=randomInteger(min_i,max_index['i']);
+    object_j=randomInteger(min_j,max_index['j']);
+
+    $('td[i="'+object_i+'"][j="'+object_j+'"]').addClass(object);
+
+    localStorage.setItem(object+'_i',object_i);
+    localStorage.setItem(object+'_j',object_j);
+});
+
+//Подбор путей
+const PLUS=1;
+const MINUS=2;
+
+const I=1;
+const J=2;
+
+$('#find-way-button').on('click',function(){
+
+    cat_i=$('td.cat').attr('i'); 
+    cat_j=$('td.cat').attr('j');
+
+    sign=randomInteger(PLUS, MINUS);
+    side=randomInteger(I, J);
+
+    max_index=getMaxIndex();
+
+    new_index=countNewIndex(cat_i,cat_j,sign,side);
+    console.log(new_index);
+    while(new_index['i']<1 || new_index['i']>max_index['i'] || new_index['j']<1 || new_index['j']>max_index['j'])
+    {
+        new_index=countNewIndex(cat_i,cat_j,sign,side);
+        console.log(new_index);
+    }
+
+    
+
+    setCatToCoordinates(new_index['i'],new_index['j']);
+});
+
+
+function countNewIndex(start_i,start_j,sign,side)
+{
+    i=start_i;
+    j=start_j;
+
+    if(sign==PLUS)
+    {
+        if(side==I)
+        {
+            i=Number.parseInt(start_i)+1;
+        }
+        if(side==J)
+        {
+            j=Number.parseInt(start_j)+1;
+        }
+    }
+
+    if(sign==MINUS)
+    {
+        if(side==I)
+        {
+            i=Number.parseInt(start_i)-1;
+        }
+        if(side==J)
+        {
+            j=Number.parseInt(start_j)-1;
+        }
+    }
+
+
+
+    new_index=[];
+    new_index['i']=i;
+    new_index['j']=j;
+
+    return new_index;
+}
+
+
+$(function(){
+    setCatToCoordinates(1,1);
+
+    food_i=localStorage.getItem('food_i');
+    food_j=localStorage.getItem('food_j');
+    setFoodToCoordinates(food_i,food_j);
+})
